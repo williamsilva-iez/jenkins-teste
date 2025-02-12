4db63c524b0abc2ffa07b84572e75e509109dcfc
williamsilva-iez <william.silva@iez.com.br>
2025-02-12 09:01:09 -0300

    feat: Alterando titulo do App para testes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Jenkins Demo1',
+      title: 'Flutter Jenkins Demo2',
       theme: ThemeData(
         // This is the theme of your application.
         //
